abd83209eb1835fb1515814a3233ce8f1e2b15f1
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2016-02-18 23:25:26 +0100

    Added Protocol

--- src/com/prgguru/jersey/Constants.java
+++ src/com/prgguru/jersey/Constants.java
@@ -2,10 +2,11 @@ package com.prgguru.jersey;
 //Change these parameters according to your DB
 public class Constants {
 	//public static String dbClass = "com.mysql.jdbc.Driver";
-	public static String dbClass = "org.sqlite.JDBC";
-	private static String dbName= "//usr/share/tomcat/webapps/useraccount/WEB-INF/classes/useraccount.db";
+	public static String dbClass = "org.sqlite.JDBC"; //SQLite
+	private static String dbName= "//usr/share/tomcat/webapps/useraccount/WEB-INF/classes/useraccount.db"; //SQLite
+	//private static String dbName= "useraccount";
 	//public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
-	public static String dbUrl = "jdbc:sqlite:"+dbName;
+	public static String dbUrl = "jdbc:sqlite:"+dbName; //SQLite
 	public static String dbUser = "root";
 	public static String dbPwd = "password";
 }
