648f22429eef20408bf31bc9b1a43da2afdc11f4
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2016-02-18 11:26:47 +0100

    Setup deployment and changed DB constants

--- src/com/prgguru/jersey/Constants.java
+++ src/com/prgguru/jersey/Constants.java
@@ -2,7 +2,7 @@ package com.prgguru.jersey;
 //Change these parameters according to your DB
 public class Constants {
 	public static String dbClass = "com.mysql.jdbc.Driver";
-	private static String dbName= "users";
+	private static String dbName= "useraccount";
 	public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
 	public static String dbUser = "root";
 	public static String dbPwd = "password";
